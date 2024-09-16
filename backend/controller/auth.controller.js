423e751b1bed260f31f22cd40aea26969d6e00fb
Aritra Bera <aritrabera69@gmail.com>
2024-09-16 23:45:59 +0530

    schema

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,12 +1,15 @@
+
+
 const signup = async (req, res) => {
     res.send("signup route")
 }
+
 const login = async (req, res) => {
     res.send("login route")
 }
+
 const logout = async (req, res) => {
     res.send("logout route")
 }
-
 
 module.exports = { signup, login, logout }
---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
+
+}, { timestamps: true })
+
+const userModel = new mongoose.Model('User', userSchema);
+
+module.exports = userModel;
