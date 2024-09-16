09f0f8c1b2ec6d1ac45ea18658e5a8251d04d6c8
Aritra Bera <aritrabera69@gmail.com>
2024-09-17 00:03:50 +0530

    authController

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,11 +1,24 @@
-
+const userModel = require('../models/user.model')
 
 const signup = async (req, res) => {
-   try {
+    const { name, email, password } = req.body;
+
+    try {
+        //For input all the required fields
+        if (!name || !email || !password) {
+            throw new Error("All fields must be required");
+        }
+
+        //Check if email already exists
+        const userAlreadyExists = await userModel.findOne({ email });
+        if (userAlreadyExists) {
+            return res.status(400).json({ success: false, message: "Email already exists" });
+        }
+
     
-   } catch (error) {
-    
-   }
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
 }
 
 const login = async (req, res) => {
