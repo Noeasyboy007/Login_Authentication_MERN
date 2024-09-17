90ba23507363fef98b950d5385622d475435342a
Aritra Bera <aritrabera69@gmail.com>
2024-09-17 19:32:16 +0530

    authcontroller _ reset password

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,12 +1,14 @@
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userModel = require('../models/user.model');
 const generateVerificationToken = require('../utils/generateVerificationToken');
 const generateTokenAndSetCookie = require('../utils/generateTokenAndSetCookie');
 const { sendVerificationmail, sendWelcomemail } = require('../mailtrap/email');
 
-// For Signup User
+// FOR SIGNUP USER
 const signup = async (req, res) => {
+    // extract the eamil address,password,name field from the body
     const { name, email, password } = req.body;
 
     try {
@@ -57,11 +59,12 @@ const signup = async (req, res) => {
     } catch (error) {
         res.status(400).json({ success: false, message: error.message });
     }
-}
+};
 
-// For Verify Email Address
+// FOR VERIFY EMAIL ADDRESS
 const verifyEmail = async (req, res) => {
-    // passes the verification code form frontend the recived code in email address
+    // extract the verification code from the body
+    // passes the verification code form frontend the recived code in email address and store in database()
     const { code } = req.body;
     try {
         const user = await userModel.findOne({
@@ -95,10 +98,11 @@ const verifyEmail = async (req, res) => {
         console.log("Error: " + error.message);
     }
 
-}
+};
 
 // FOR LOGIN USER
 const login = async (req, res) => {
+    // extract the eamil address from the body wich store in database
     const { email, password } = req.body;
     try {
         // Check if user have accout or valid email address
@@ -136,10 +140,10 @@ const login = async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
-        console.log("Error: " + error.message);
+        console.log("Error in login user: " + error.message);
     }
 
-}
+};
 
 // FOR LOGOUT
 const logout = async (req, res) => {
@@ -147,6 +151,44 @@ const logout = async (req, res) => {
     res.status(200).json({ success: true, message: "Logout successful" });
     console.log("Logout successful");
 
-}
+};
 
-module.exports = { signup, login, logout, verifyEmail }
+// FOR FORGOT PASSWORD
+const forgotPassword = async (req, res) => {
+    // extract the eamil address from the body wich store in database
+    const { email } = req.body;
+
+    try {
+        // Check if user have accout or valid email address
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ success: false, message: "Email not found" });
+        }
+
+        // generate a new reset token
+        const resetToken = crypto.randombytes(20).toString('hex');
+
+        // Token expiration
+        const resetTokenExpiresAt = Date.now() + 10 * 60 * 1000; // 10 minutes
+
+        // update the database
+        user.resetPasswordToken = resetToken;
+        user.resetPasswordExpiresAt = resetTokenExpiresAt;
+
+        // save the updated user
+        await user.save();
+
+        // Send Reset password link with token in email notification to user
+        await sendResetPasswordEmail(user.email, `${process.env.CLIENT_URL}/resetPassword/${resetToken}`);
+
+        // For response Messege Code
+        res.status(200).json({ success: true, message: "Reset Password link sent to your email" });
+        console.log("Reset Password link sent to your email");
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+        console.log("Error in forgot password: " + error.message);
+    }
+};
+
+module.exports = { signup, login, logout, verifyEmail, forgotPassword }
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const { signup, login, logout, verifyEmail } = require("../controller/auth.controller")
+const { signup, login, logout, verifyEmail, forgotPassword } = require("../controller/auth.controller")
 
 const router = express.Router();
 
 router.post("/signup", signup)
+
 router.post("/verify-email", verifyEmail)
+
 router.post("/login", login)
+
 router.post("/logout", logout)
+
+router.post("forgot-password", forgotPassword)
 
 
 module.exports = router;
