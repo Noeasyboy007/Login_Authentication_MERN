20563e043953eefad02e7af8f8483c11beacfe0d
Aritra Bera <aritrabera69@gmail.com>
2024-09-22 02:54:18 +0530

    build authSignup in store

---
+++ frontend/src/store/authStore.js
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000/api/auth/'
+export const useAuthStore = create((set) => ({
+    user: null,
+    isAuthenticated: false,
+    error: null,
+    isLoading: false,
+    isCheckingAuth: true,
+
+    // Signup Function
+    signup: async (email, password, name) => {
+        set({ isLoading: true, error: null });
+
+        try {
+            const response = await axios.post(`${API_URL}/signup`, { email, password, name });
+            set({ user: response.data.user, isAuthenticated: true, isLoading: false });
+        } catch (error) {
+            set({error: error.response.message ||"Error Signup", isLoading: false});
+            throw error
+        }
+    }
+}))
